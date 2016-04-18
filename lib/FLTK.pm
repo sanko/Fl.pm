1b6b9c89e7d83516b44d29f273e38ccb0638d5c1
Sanko Robinson <sanko@cpan.org>
2016-04-18 14:50:18 -0400

    Case

--- lib/FLTK.pm
+++ lib/FLTK.pm
@@ -7,7 +7,7 @@ use Exporter 5.57 'import';
 our $VERSION     = '0.99.00';
 our %EXPORT_TAGS = ('all' => []);
 our @EXPORT_OK   = (@{$EXPORT_TAGS{'all'}});
-XSLoader::load('Fltk', $VERSION);
+XSLoader::load('FLTK', $VERSION);
 
 sub dl_load_flags {0} # Prevent DynaLoader from complaining and croaking
 
