e83b97ad696055fb7599509a485c458834f2406b
Sanko Robinson <sanko@cpan.org>
2016-04-21 18:38:52 -0400

    bjk

--- inc/MBFLTKExt.pm
+++ inc/MBFLTKExt.pm
@@ -164,7 +164,7 @@ package inc::MBFLTKExt;
                     extra_linker_flags => # ' -Wl ' . '-L' . $alien->library_path . ' ' . $alien->ldflags() . ' -lstdc++'
                         ['-L' . $AF->library_path(),
                          $AF->ldflags(qw[images gl]),
-                         ' -lstdc++' . " -Wl,--gc-sections -fPIC -shared"
+                         ' -lstdc++' #. " -Wl,--gc-sections -fPIC -shared"
                         ],
                 );
                 printf "%s\n",
