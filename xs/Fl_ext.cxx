07a9a45ee683276f432f0b145ee0a3f504299df8
Sanko Robinson <sanko@cpan.org>
2016-05-01 19:52:01 -0400

    New Fl::Chart widget with tests and example script!

--- xs/Fl_ext.cxx
+++ xs/Fl_ext.cxx
@@ -4,6 +4,7 @@
 #include <FL/Fl_Group.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Window.H>
+#include <FL/Fl_Chart.H>
 
 const char * object2package (Fl_Widget * w) {
     /*Remember to add _most_ specific classes first*/
@@ -11,5 +12,6 @@ const char * object2package (Fl_Widget * w) {
          if ( dynamic_cast<Fl_Box    *>(w) ) { package = "Fl::Box";    }
     else if ( dynamic_cast<Fl_Window *>(w) ) { package = "Fl::Window"; }
     else if ( dynamic_cast<Fl_Group  *>(w) ) { package = "Fl::Group";  }
+    else if ( dynamic_cast<Fl_Chart *>(w) ) { package = "Fl::Chart"; }
     return package;
 }
