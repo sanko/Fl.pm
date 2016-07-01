70f9c27d7b47c9aaee943d434c950aa44978776a
Sanko Robinson <sanko@cpan.org>
2016-07-01 09:08:21 -0400

    Checking in changes prior to tagging of version 0.99.14.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index efecec6..48369c2 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,7 +2,10 @@ Revision history for Perl extension Fl
    
     {{$NEXT}}
    
    +0.99.14 2016-07-01T13:04:26Z
    +
         - Fix crash when args are set with $widget->callback(...)
    +    - Custom widgets are now reblessed into the correct package in callbacks
    
     0.99.13 2016-06-27T15:31:43Z

--- lib/Fl.pm
+++ lib/Fl.pm
@@ -2,7 +2,7 @@ package Fl;
 use 5.008001;
 use strict;
 use warnings;
-our $VERSION = '0.99.13';
+our $VERSION = '0.99.14';
 use XSLoader;
 use vars qw[@EXPORT_OK @EXPORT %EXPORT_TAGS];
 use Exporter qw[import];
